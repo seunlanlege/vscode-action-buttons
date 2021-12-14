67c4ef104fd01eef0dc03c7b749d3e32a033677e
Addison Elliott <addison.elliott@gmail.com>
2021-12-14 07:57:28 -0600

    Add tooltip option for commands (#66)
    * nit
    
    * Add type
    
    * Add tooltip logic
    
    * Update description
    
    Co-authored-by: Addison Elliott <addisone@becs.com>

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,17 @@
-export interface RunButton {
+export interface CommandOpts {
 	cwd?: string
 	command: string
-	vsCommand: string
 	singleInstance?: boolean
 	name: string
+	tooltip: string
 	color: string
 	focus?: boolean
 	useVsCodeApi?: boolean
+}
+
+export interface ButtonOpts {
+	command: string
+	tooltip: string
+	name: string
+	color: string
 }
